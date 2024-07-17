3ec4889d1028ef1b344f9bc6992f790b75f26786
raysonlopez <rslopez6@espe.edu.ec>
2024-07-17 12:28:45 -0500

    Juan juian

---
+++ P2Lab2LopezRayson/src/main/java/elmain/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elmain;
+import Interface.Login;
+/**
+ *
+ * @author Rayson
+ */
+public class Main {
+    public static void main(String[]args){
+               // Ejecutar la GUI en el hilo de eventos de Swing
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Crear una nueva instancia de la ventana de login y hacerla visible
+                new Login().setVisible(true);
+            }
+        });
+    }
+}
