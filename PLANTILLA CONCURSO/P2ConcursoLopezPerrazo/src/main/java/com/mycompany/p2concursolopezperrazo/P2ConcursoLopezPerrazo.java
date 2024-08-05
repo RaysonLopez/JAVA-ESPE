d7d9edb03c2f482d6f1a7c0ace53ad23be16ce32
raysonlopez <rslopez6@espe.edu.ec>
2024-08-04 22:33:43 -0500

    Plantilla concurso programacion

---
+++ PLANTILLA CONCURSO/P2ConcursoLopezPerrazo/src/main/java/com/mycompany/p2concursolopezperrazo/P2ConcursoLopezPerrazo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p2concursolopezperrazo;
+
+/**
+ *
+ * @author Rayson
+ */
+public class P2ConcursoLopezPerrazo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
